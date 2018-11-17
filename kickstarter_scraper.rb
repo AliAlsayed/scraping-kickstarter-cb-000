cda6d408c5389f17c01636363f1f0548c13c61f0
Ali Alsayed <a_alsayed87@hotmail.com>
2018-11-17 15:55:39 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,8 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   projects = {}
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title] = {}
   end
   projects
   #binding.pry
