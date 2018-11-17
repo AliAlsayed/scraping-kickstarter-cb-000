02e46226538c456bc53efb34be83104a7ce6cb43
Ali Alsayed <a_alsayed87@hotmail.com>
2018-11-17 15:55:51 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,7 @@ def create_project_hash
   projects = {}
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title] = {}
+    projects[title.to_sym] = {}
   end
   projects
   #binding.pry
