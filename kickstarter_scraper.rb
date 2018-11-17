d4984271d38b5096208038a6c5c524d85f7b3424
Ali Alsayed <a_alsayed87@hotmail.com>
2018-11-17 15:48:23 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,4 +11,6 @@ end
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+#location: project.css('ul.project-meta li a').text
+#percent_funded: project.css('ul.project-stats li strong').text
 create_project_hash
